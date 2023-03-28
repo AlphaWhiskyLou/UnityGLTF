d6d8acee6db19c42cf786417260e66b84aa20892
Tianyu Lou <alphawhiskylou@gmail.com>
2023-03-28 22:13:34 +0800

    Add decompile info

--- GLTFSerialization/GLTFSerialization/Schema/Scene.cs
+++ GLTFSerialization/GLTFSerialization/Schema/Scene.cs
@@ -1,4 +1,10 @@
-﻿using Newtonsoft.Json;
+﻿// Decompiled with JetBrains decompiler
+// Type: GLTF.Schema.Scene
+// Assembly: GLTFSerialization, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: B3116234-21EE-4DC5-AA39-14DAC54A1959
+// Assembly location: D:\UnityProjects\AISceneGeneration\Assets\AISceneGeneration\Dependencies\Sketchfab For Unity\Dependencies\Libraries\SketchfabGLTFSerialization.dll
+
+using Newtonsoft.Json;
 using System.Collections.Generic;
 
 namespace GLTF.Schema
